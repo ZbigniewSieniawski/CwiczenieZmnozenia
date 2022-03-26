70cccb7963a250a20897e5fbe5d2575a166ba991
Zbigniew Sieniawski <tesciu68@gmail.com>
2022-03-26 21:05:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NewConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = Multiply(2, 4);
+            Console.WriteLine(total);
+        }
+
+        public static int Multiply(int multiplicand, int factor)
+        {
+            int result = 0;
+            
+            for (int i = 1; i <= Math.Abs(factor); i++)
+            {
+                result = result + multiplicand;
+            }
+
+            return result;
+        }
+    }
+}
